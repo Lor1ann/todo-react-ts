676217cd73683ff1a7f36939e87056b17da4e7e8
Eugene <hurakane122@gmail.com>
2022-01-13 20:25:22 +0300

    Сделано ДЗ 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,70 @@
-import './App.css';
-import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
-import { AddField } from './components/AddField';
-import { Item } from './components/Item';
+import "./App.css";
+import React from "react";
+import { Paper, Divider, Button, List, Tabs, Tab } from "@mui/material";
+import { AddField } from "./components/AddField";
+import { Item } from "./components/Item";
 
 function App() {
+  type Task = {
+    id: number;
+    text: string;
+    complited: boolean;
+  };
+
+  function reduser(
+    state: Array<Task>,
+    action: { type: string; text?: string; complited: boolean }
+  ) {
+    if (action.type === "ADD_TASK") {
+      return [
+        ...state,
+        {
+          id: state.length,
+          text: action.text,
+          complited: action.complited,
+        },
+      ] as Array<Task>;
+    }
+
+    return state;
+  }
+
+  const [text, setText] = React.useState<string>("");
+  const [checkAddField, setCheckAddField] = React.useState(false);
+
+  function getText(e: any) {
+    setText(e.target.value);
+  }
+
+  function getCheck() {
+    setCheckAddField(!checkAddField);
+  }
+
+  const [state, dispatch] = React.useReducer(reduser, [] as Array<Task>);
+
+  function addTask() {
+    dispatch({
+      type: "ADD_TASK",
+      text: text,
+      complited: checkAddField,
+    });
+    setText("");
+    setCheckAddField(false);
+  }
+
   return (
     <div className="App">
-  <Paper className="wrapper">
+      <Paper className="wrapper">
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField />
+        <AddField
+          checked={checkAddField}
+          addTask={addTask}
+          getText={getText}
+          text={text}
+          getCheck={getCheck}
+        />
         <Divider />
         <Tabs value={0}>
           <Tab label="Все" />
@@ -19,7 +73,11 @@ function App() {
         </Tabs>
         <Divider />
         <List>
-          <Item />
+          {state.map((obj: any) => {
+            return (
+              <Item text={obj.text} key={obj.id} checked={obj.complited} />
+            );
+          })}
         </List>
         <Divider />
         <div className="check-buttons">
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { IconButton, Checkbox, ListItem, Typography } from '@mui/material';
+import React from "react";
+import { IconButton, Checkbox, ListItem, Typography } from "@mui/material";
 
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-
-export const Item = () => {
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+export const Item = (props: { text: string; checked: boolean }) => {
   return (
     <ListItem>
       <div className="d-flex item">
-        <Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} />
-        <Typography className="item-text"></Typography>
+        <Checkbox
+          checked={props.checked}
+          icon={<RadioButtonUncheckedIcon />}
+          checkedIcon={<CheckCircleIcon />}
+        />
+        <Typography className="item-text">{props.text}</Typography>
         <div className="item-buttons d-flex">
           <IconButton>
             <EditIcon style={{ fontSize: 20 }} />
