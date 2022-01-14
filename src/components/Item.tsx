3ca4a5dd4eacd937c729701890a7ba8948c92b99
Eugene <hurakane122@gmail.com>
2022-01-14 21:59:02 +0300

    Сделал задание 2

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,7 +5,12 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-export const Item = (props: { text: string; checked: boolean }) => {
+export const Item = (props: {
+  text: string;
+  checked: boolean;
+  onDelete: any;
+  id: number;
+}) => {
   return (
     <ListItem>
       <div className="d-flex item">
@@ -19,7 +24,7 @@ export const Item = (props: { text: string; checked: boolean }) => {
           <IconButton>
             <EditIcon style={{ fontSize: 20 }} />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={() => props.onDelete(props.id)}>
             <DeleteOutlineIcon style={{ fontSize: 20 }} />
           </IconButton>
         </div>
